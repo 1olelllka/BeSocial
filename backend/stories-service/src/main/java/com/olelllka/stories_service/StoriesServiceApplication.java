8c5f00f550f462c9b705de3a2190e2f176e28d10
Oleshka <olehit32@gmail.com>
2024-12-01 20:06:09 +0100

    Added stories service (basic layout)

---
+++ backend/stories-service/src/main/java/com/olelllka/stories_service/StoriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.olelllka.stories_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoriesServiceApplication.class, args);
+	}
+
+}
